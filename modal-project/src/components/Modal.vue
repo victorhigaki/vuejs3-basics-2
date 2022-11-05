781f1b4946d43b2b67ee91df5df234d974993d26
Victor Higaki <victorhigaki@outlook.com>
2022-11-05 18:26:27 -0300

    named slot

--- modal-project/src/components/Modal.vue
+++ modal-project/src/components/Modal.vue
@@ -2,6 +2,9 @@
     <div @click.self="closeModal" class="backdrop">
         <div class="modal" :class="{ sale: theme === 'sale' }">
             <slot></slot>
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
         </div>
     </div>
 </template>
