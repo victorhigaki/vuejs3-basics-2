1f83c3308b935e985cbf9f959bbf25351a4be96c
Victor Higaki <victorhigaki@outlook.com>
2022-11-05 16:38:02 -0300

    emit

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <h1>{{ title }}</h1>
-  <Modal :header="header" :text="text" theme="sale" />
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+  </div>
+  <button @click="toggleModal">open modal</button>
 </template>
 
 <script>
@@ -13,7 +17,13 @@ export default {
     return {
       title: 'My First Vue App :)',
       header: 'Sign up for the Giveaway!',
-      text: 'Grab your ninja swag for half price!'
+      text: 'Grab your ninja swag for half price!',
+      showModal: false,
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
     }
   }
 }
