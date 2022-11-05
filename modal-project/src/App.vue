72b6a76318d944acf11965f49500903adb04fcbe
Victor Higaki <victorhigaki@outlook.com>
2022-11-05 18:05:49 -0300

    slot

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -2,7 +2,10 @@
   <h1>{{ title }}</h1>
   <p>Welcome...</p>
   <div v-if="showModal">
-    <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+    <Modal theme="sale" @close="toggleModal">
+      <h1>Ninja Givaway!</h1>
+      <p>Grab your ninja swag for half price!</p>
+    </Modal>
   </div>
   <button @click.alt="toggleModal">open modal (alt)</button>
 </template>
